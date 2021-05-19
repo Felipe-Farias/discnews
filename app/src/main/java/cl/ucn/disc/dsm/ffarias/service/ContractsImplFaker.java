900ad6f4b6736f7e32eaa6690fe0baed9ca96080
felipe farias <feliep.farias01@alumnos.ucn.cl>
2021-05-19 00:53:30 -0400

    test(Contracts): test the contracts

--- app/src/main/java/cl/ucn/disc/dsm/ffarias/service/ContractsImplFaker.java
+++ app/src/main/java/cl/ucn/disc/dsm/ffarias/service/ContractsImplFaker.java
@@ -5,12 +5,21 @@
 package cl.ucn.disc.dsm.ffarias.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import cl.ucn.disc.dsm.ffarias.model.News;
 
 public final class ContractsImplFaker implements Contracts{
 
+    private final List<News> listNews = new ArrayList<>();
+
+    /**
+     * The constructor.
+     */
+    public ContractsImplFaker() {
+        // Nothing here.
+    }
 
     /**
      * Get the list of news.
@@ -20,16 +29,36 @@ public final class ContractsImplFaker implements Contracts{
      */
     @Override
     public List<News> retrieveNews(final Integer size) {
-        return new ArrayList<>();
+        // Preconditions
+        if(size <= 0){
+            throw new IllegalArgumentException("Size cannot be zero or negative");
+        }
+        // Return the unmodifiable list
+        return Collections.unmodifiableList(this.listNews);
     }
 
     /**
-     * Save one news to the System
+     * Save one news to the System.
      *
      * @param news
      */
     @Override
     public void save(final News news) {
+
+        // Nullity test
+        if(news == null){
+            throw new IllegalArgumentException("Need news != null");
+        }
+
+        // No duplicate allowed
+        for(News n : this.listNews){
+
+            if(n != null && n.getId().equals(news.getId())){
+                throw new IllegalArgumentException("News is already in the list");
+            }
+        }
+
+        this.listNews.add(news);
 
     }
 }
---
+++ app/src/test/java/cl/ucn/disc/dsm/ffarias/service/TestContracts.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) Felipe Farias Espinoza
+ */
+
+package cl.ucn.disc.dsm.ffarias.service;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import cl.ucn.disc.dsm.ffarias.model.News;
+
+public final class TestContracts {
+
+    /**
+     * Testing the constructor
+     */
+    //@Test
+    public void testConstructor(){
+
+        // Call the constructor
+        Contracts contracts = new ContractsImplFaker();
+        // Assertions.assertNotNull(contracts, "Contracts null!");
+
+    }
+
+    //@Test
+    public void testSave(){
+
+        // The contracts implementation
+        Contracts contracts = new ContractsImplFaker();
+
+       // 1 news
+        News news = new News(
+                "The tittle",
+                "The source",
+                "The author",
+                "The url",
+                "The image url",
+                "The description",
+                "The content",
+                ZonedDateTime.now(ZoneId.of("-3")));
+        //save into the backend
+        contracts.save(news);
+
+        // null news
+      //  Assertion.assertThrows(IllegalArgumentException.class, () -->{
+      //      contracts.save(null);
+      //  });
+
+        // verify the size of the list
+       // Assertions.assertEquals(1, contracts.retrieveNews(10).size());
+
+
+        // Save into the backend v2.0
+        //  Assertion.assertThrows(IllegalArgumentException.class, () -->{
+        //      contracts.save(news);
+        //  });
+    }
+}
