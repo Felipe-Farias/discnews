afc465f62784270d8fe8c0b89258baa0e92fb821
Felipe-Farias <felipe.farias01@alumnos.ucn.cl>
2021-05-27 12:53:32 -0400

    refactor(Test): testing the phone interface

--- app/src/main/java/cl/ucn/disc/dsm/ffarias/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/ffarias/MainActivity.java
@@ -3,13 +3,36 @@ package cl.ucn.disc.dsm.ffarias;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+/**
+ * The main class.
+ * @author Felipe Farias-Espinoza.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     *
+     * @param savedInstanceState The instance.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TheNews", "onCreate !!");
 
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("TheNews", "onStart !!!");
+    }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("TheNews", "onResume !!!");
     }
 }
